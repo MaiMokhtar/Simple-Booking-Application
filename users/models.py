bda86661b5d69dd33b293c96b3f8ae6cd2f11e0c
Mai Mokhtar <maimokhtar996@gmail.com>
2023-02-28 22:13:04 +0200

    ENH: delete user_type from User model

--- users/models.py
+++ users/models.py
@@ -1,15 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-USER_TYPES = (
-    ('studio_owner', 'Studio Owner'),
-    ('employee', 'Employee'),
-    ('customer', 'Customer'),
-)
-
 
 class User(AbstractUser):
-    user_type = models.CharField(max_length=20, choices=USER_TYPES, default='customer')
     is_studio_owner = models.BooleanField(default=False)
     is_employee = models.BooleanField(default=False)
     is_customer = models.BooleanField(default=False)
