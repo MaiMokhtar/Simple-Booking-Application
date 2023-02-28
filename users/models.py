c6c83123519cfd2161f76eaae82a9562826658f3
Mai Mokhtar <maimokhtar996@gmail.com>
2023-02-28 15:37:42 +0200

    DEV: add is_customer for user model

--- users/models.py
+++ users/models.py
@@ -12,3 +12,4 @@ class User(AbstractUser):
     user_type = models.CharField(max_length=20, choices=USER_TYPES, default='customer')
     is_studio_owner = models.BooleanField(default=False)
     is_employee = models.BooleanField(default=False)
+    is_customer = models.BooleanField(default=False)
