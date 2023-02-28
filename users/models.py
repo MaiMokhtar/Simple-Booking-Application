ea4f54a9a8143f1245f8155de8c61d5000fa56c8
Mai Mokhtar <maimokhtar996@gmail.com>
2023-02-28 23:33:54 +0200

    DOC: write doc string for models.py in users app

--- users/models.py
+++ users/models.py
@@ -1,8 +1,17 @@
+"""Module for defining Django models related to user."""
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
+    """Custom user model that extends the AbstractUser class.
+
+    Attributes:
+        is_studio_owner (bool): Indicates if the user is a studio owner.
+        is_employee (bool): Indicates if the user is an employee of a studio.
+        is_customer (bool): Indicates if the user is a customer of a studio.
+    """
     is_studio_owner = models.BooleanField(default=False)
     is_employee = models.BooleanField(default=False)
     is_customer = models.BooleanField(default=False)
